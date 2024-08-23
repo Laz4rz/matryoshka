37ffbbb3200e1874b9960f4437b1978940913156
Laz4rz <62252332+Laz4rz@users.noreply.github.com>
2024-08-23 15:32:59 +0200

    see how sentence_transformers outputs stuff for mteb

---
+++ inference_st.py
@@ -0,0 +1,7 @@
+from sentence_transformers import SentenceTransformer
+sentences = ["This is an example sentence", "Each sentence is converted"]
+
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+embeddings = model.encode(sentences)
+print(embeddings)
+
