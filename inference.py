d6915d7fb1bbfeb49444c29b35588c38204d7b04
Laz4rz <62252332+Laz4rz@users.noreply.github.com>
2024-08-23 14:18:34 +0200

    add comparison to matryoshka

--- inference.py
+++ inference.py
@@ -1,6 +1,9 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn.functional as F
+import numpy as np
+
+from matryoshka import Matryoshka
 
 #Mean Pooling - Take attention mask into account for correct averaging
 def mean_pooling(model_output, attention_mask):
@@ -11,17 +14,23 @@ def mean_pooling(model_output, attention_mask):
 
 # Sentences we want sentence embeddings for
 sentences = ['This is an example sentence', 'Each sentence is converted']
+sentences = ["sentence", "not a sentence"]
 
 # Load model from HuggingFace Hub
 tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+matryoshka = Matryoshka(matryoshka_dim=64)
 model = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
 
 # Tokenize sentences
 encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt')
+print("Encoded input shape:", encoded_input['input_ids'].shape)
 
 # Compute token embeddings
 with torch.no_grad():
     model_output = model(**encoded_input)
+    matryoshka_output = matryoshka(**encoded_input)
+
+assert torch.allclose(model_output[0], matryoshka_output[0])
 
 # Perform pooling
 sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
@@ -30,4 +39,4 @@ sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask']
 sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
 
 print("Sentence embeddings:")
-print(sentence_embeddings)
+print(np.dot(sentence_embeddings, sentence_embeddings.T))
