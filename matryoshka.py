0729e60af4248aed1017431d82c04ab51cc3c44b
Laz4rz <62252332+Laz4rz@users.noreply.github.com>
2024-08-23 15:32:38 +0200

    better matryoshka approach

--- matryoshka.py
+++ matryoshka.py
@@ -6,10 +6,16 @@ from transformers import AutoTokenizer, AutoModel
 
 class Pooler(nn.Module):
 
-    def __init__(self, hidden_size):
+    def __init__(self, hidden_size=None, base_model=None):
         super(Pooler, self).__init__()
         self.dense = nn.Linear(hidden_size, hidden_size)
         self.activation = nn.Tanh()
+        if base_model is not None:
+            with torch.no_grad():
+                weigths = model.state_dict()["0.auto_model.pooler.dense.weight"][:hidden_size, :hidden_size]
+                biases = model.state_dict()["0.auto_model.pooler.dense.bias"][:hidden_size]
+                self.dense.weight.copy_(weights)
+                self.dense.bias.copy_(biases)
 
     def forward(self, inputs):
         cls_tokens = inputs[:, 0]
@@ -18,38 +24,49 @@ class Pooler(nn.Module):
         return pooled_output
 
 
+class Normalizer(nn.Module):
+
+    def __init__(self, p=2, dim=1):
+        super(Normalizer, self).__init__()
+        self.p = p
+        self.dim = dim
+
+    def forward(self, inputs):
+        return F.normalize(inputs, p=self.p, dim=self.dim)
+
+
+class BaseModel(nn.Module):
+
+    def __init__(self, model_name="sentence-transformers/all-MiniLM-L6-v2"):
+        super(BaseModel, self).__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModel.from_pretrained(model_name)
+
+    def forward(self, **kwargs):
+        return self.model(**kwargs)
+
+
 class Matryoshka(nn.Module):
 
     def __init__(self,
                  model_name="sentence-transformers/all-MiniLM-L6-v2",
-                 matryoshka_dim=None,
-                 raw_pooler=True):
-        super(Matryoshka, self).__init__()
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModel.from_pretrained(model_name)
-        self.matryoshka_dim = model.config.hidden_size if matryoshka_dim is None else matryoshka_dim
-        self.matryoshka = False
-
+                 matryoshka_dim=64,
+                 raw_pooler=True,
+                 only_hidden_states=False):
         assert self.model.config.hidden_size >= matryoshka_dim, \
             f"Model hidden size ({self.model.config.hidden_size}) must be greater than or equal to matryoshka_dim ({matryoshka_dim})"
-        if self.model.config.hidden_size > matryoshka_dim:
-            self.matryoshka_dim = matryoshka_dim
-            self.matryoshka = True
-            if raw_pooler:
-                self.pooler = Pooler(self.matryoshka_dim)
-            else:
-                # this needs some smart matrix dim reduction
-                # i can do it stupid but dunno if I should
-                self.pooler = self.model.pooler
+
+        super(Matryoshka, self).__init__()
+
+        self.matryoshka_dim = matryoshka_dim
+
+        self.model = BaseModel(model_name)[0]  # not pooled outputs
+        self.pooler = Pooler(hidden_size=self.matryoshka_dim,
+                             base_model=self.model)
+        self.normalizer = Normalizer()
 
     def forward(self, **kwargs):
-        base_outputs = self.model(**kwargs)
-        if self.matryoshka:
-            base_outputs["last_hidden_state"] = base_outputs[
-                "last_hidden_state"][:, :, :self.matryoshka_dim]
-            base_outputs["pooler_output"] = self.pooler(
-                base_outputs["last_hidden_state"])
-        print("Matryoshka output shapes:",
-              base_outputs["last_hidden_state"].shape,
-              base_outputs["pooler_output"].shape)
-        return base_outputs
+        model_output = self.model(**kwargs)
+        pooled_output = self.pooler(model_output)
+        normalized_output = self.normalizer(pooled_output)
+        return normalized_output
